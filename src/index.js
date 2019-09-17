a179e88af25dd98a9808d0caab92e7d55cbd9f80
henrypadua <henrybpj@live.com>
2019-09-16 22:57:09 -0300

    Primeiro Componente

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-const soma = (a, b) => a + b;
+import React, {Component, Fragment} from "react";
+import { render } from "react-dom";
 
-alert(soma(5, 3));
+class Button extends Component {
+  render() {
+    return (
+      <a href="" onClick={this.props.onClickss}>
+      {this.props.children}
+        </a>
+    );
+  }
+}
+
+class App extends Component {
+  handleClick() {
+    alert("Botao clicado!ssss");
+  }
+
+  render() {
+    return(
+    <Fragment>
+      <h1>Hello Rocketseat</h1>
+      <Button onClick={this.handleClick}>Enviar</Button>
+    </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
